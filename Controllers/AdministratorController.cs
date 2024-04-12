9d740fb7e53b49fd2d034ce23fd43f59d2ef433f
Marco Scipinotti <marco.scipinotti@accenture.com>
2024-04-12 13:42:38 +0200

    DeleteAccount

--- Controllers/AdministratorController.cs
+++ Controllers/AdministratorController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Net;
+using System.Net.Http.Json;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json;
 using WebAPP.Extensions;
 using WebAPP.Infrastructure.Models;
 using WebAPP.MiddlewareFactory;
@@ -71,7 +76,7 @@ namespace WebAPP.Infrastructure.Controllers
             {
                 _httpClient.SetTokens(accountsDto);
                 var httpResponseMessage = await _httpClient.PostAsJsonAsync($"{GlobalParameters.GlobalParameters.Config.GetValue<string>("apiURL")!}Administrator/ModifyAccount", accountsDto, _ct.Token);
-                if (httpResponseMessage.StatusCode != HttpStatusCode.OK) throw new BadHttpRequestException(httpResponseMessage.Content.ReadAsStream().ToString() ?? string.Empty);
+                if (httpResponseMessage.StatusCode != HttpStatusCode.OK) throw new BadHttpRequestException(httpResponseMessage.Content.ReadAsStringAsync().ToString() ?? string.Empty);
                 return await Task.Run(async () => View(await httpResponseMessage.Content.ReadFromJsonAsync<AccountsDto>()));
             }
             catch (Exception ex)
@@ -83,21 +88,25 @@ namespace WebAPP.Infrastructure.Controllers
         }
 
         [HttpPost("DeleteAccounts")]
-        public async Task<IActionResult> DeleteAccountsAsync(AccountDto account)
+        public async Task<IActionResult> DeleteAccountsAsync([FromBody] AccountsDto accountsDto)
         {
             try
             {
-                _httpClient.SetTokens(account);
-                var httpResponseMessage = await _httpClient.PostAsJsonAsync($"{GlobalParameters.GlobalParameters.Config.GetValue<string>("apiURL")!}Administrator/DeleteAccounts", account);
-                if (httpResponseMessage.StatusCode == HttpStatusCode.BadRequest) throw new BadHttpRequestException(httpResponseMessage.Content.ReadAsStream().ToString() ?? string.Empty);
-                return await Task.Run(async () => View(await httpResponseMessage.Content.ReadFromJsonAsync<AccountsDto>()));
+                _httpClient.SetTokens(accountsDto);
+                var httpResponseMessage = await _httpClient.PostAsJsonAsync($"{GlobalParameters.GlobalParameters.Config.GetValue<string>("apiURL")!}Administrator/DeleteAccounts", accountsDto, _ct.Token);
+                if (httpResponseMessage.StatusCode != HttpStatusCode.OK)
+                {
+                    accountsDto = await httpResponseMessage.Content.ReadFromJsonAsync<AccountsDto>();
+                    return BadRequest(accountsDto);
+                }
             }
             catch (Exception ex)
             {
                 WriteLog.WriteErrorLog(_logger, _configLogger, ex.Message);
-                account.Errors = [ex.Message];
+                accountsDto.Errors = [ex.Message];
+                return BadRequest(accountsDto);
             }
-            return View(account);
+            return await Task.Run(() => View(accountsDto));
         }
     }
 }
