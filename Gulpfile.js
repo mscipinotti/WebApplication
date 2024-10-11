97efbf4c129c5ed64836501a19afa76f3a268bb6
Marco Scipinotti <marco.scipinotti@accenture.com>
2024-10-11 17:50:10 +0200

    Fix gulfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -15,6 +15,8 @@ let paths = {
     jquery: "./wwwroot/lib/jquery/dist/"
 };
 
+let regexp = /\.(css|js)$/;
+
 paths.js = paths.webroot + "lib/custom/js/**/*.js";
 paths.minJs = paths.webroot + "js/**/*.min.js";
 
@@ -32,38 +34,24 @@ paths.bootstrapCss = paths.bootstrap + "css/**/*.css";
 
 paths.jqueryJs = paths.jquery + "js/**/*.js";
 
-// Clean js folder
-gulp.task("clean:js", async function (cb) {
-    rimraf(paths.concatJs, cb);
+gulp.task("clean", async function (cb) {
+    gulp.src([ paths.concatCss, paths.concatJs ])
+        .pipe(deletefile({
+            reg: regexp,
+            deleteMatch: true
+        }));
 });
 
-// Clean css folder
-gulp.task("clean:css", async function (cb) {
-    rimraf(paths.concatCss, cb);
-});
-
-// Clean totale
-gulp.task("Clean", gulp.series("clean:js", "clean:css"));
-
-// Minifica tutti i .js del progetto. Poi li raccoglie nella cartella js. Facendo un solo wrapper non funziona
-gulp.task("min:js", async function () {
-    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+gulp.task("add", async function () {
+    gulp.src([paths.js, "!" + paths.minJs], { base: "." })
         .pipe(concat(paths.concatJsDest))
         .pipe(uglify())
         .pipe(gulp.dest("."));
-});
-
-// Minifica tutti i .css del progetto e li raggruppa in site.min.css
-gulp.task("min:css", async function () {
-    return gulp.src([paths.css, "!" + paths.minCss], { base: "." })
+    gulp.src([paths.css, "!" + paths.minCss], { base: "." })
         .pipe(concat(paths.concatCssDest))
         .pipe(cssmin())
         .pipe(gulp.dest("."));
-});
-
-// Costruisce la js folder
-gulp.task('lib:js', async function () {
-    return gulp.src([
+    gulp.src([
         './node_modules/bootstrap/dist/js/bootstrap.min.js',
         './node_modules/jquery-ui/ui/widgets/datepicker.js',
         './node_modules/jquery-ui/ui/i18n/datepicker-en-GB.js',
@@ -73,11 +61,15 @@ gulp.task('lib:js', async function () {
         './node_modules/jquery-ui/dist/jquery-ui.min.js',
     ])
         .pipe(gulp.dest(paths.webroot + 'js'));
+    gulp.src([
+        './node_modules/bootstrap/dist/css/bootstrap.min.css',
+        './node_modules/jquery-ui/dist/themes/base/jquery-ui.min.css',
+    ])
+        .pipe(gulp.dest(paths.webroot + 'css'));
 });
 
 // Rename
 gulp.task('rename:en-GB', async function () {
-    let regexp = /\.js$/;
     gulp.src(paths.webroot + "js/datepicker-en-GB.js")
         .pipe(rename(function (path) {
             // Returns a completely new object, make sure you return all keys needed!
@@ -95,14 +87,3 @@ gulp.task('rename:en-GB', async function () {
         }));
 });
 
-// Costruisce la css folder
-gulp.task('lib:css', async function () {
-    return gulp.src([
-        './node_modules/bootstrap/dist/css/bootstrap.min.css',
-        './node_modules/jquery-ui/dist/themes/base/jquery-ui.min.css',
-    ])
-        .pipe(gulp.dest(paths.webroot + 'css'));
-});
-
-// Costruzione totale (eccetto le ommagini)
-gulp.task("Add", gulp.series(['min:js', 'min:css', 'lib:js', 'lib:css']));
